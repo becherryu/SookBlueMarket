5f4c75a97125c1ece8e08a4f6c70f60ee18b7f5f
becherryu <81617058+becherryu@users.noreply.github.com>
2024-05-17 13:14:00 +0900

    로그인, 회원가입

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import Header from "./Header"; //공통헤더
+import Register from "./pages/register";
+import Login from "./pages/login";
+import First from "./pages/first";
 
 function App() {
-  //test용
-  return <h1>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/first" element={<First />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/first.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"; // useNavigate 훅 임포트
+import "../css/LoginScreen.css";
+
+const First = () => {
+  const navigate = useNavigate(); // navigate 함수 초기화
+
+  // 회원가입 페이지로 이동하는 함수
+  const handleSignupClick = () => {
+    navigate("/register");
+  };
+
+  // 로그인 페이지로 이동하는 함수
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className="login-screen">
+      <header className="header">
+        {/* Your back arrow and menu icon will go here, you can use svg or icons from a library */}
+      </header>
+      <main className="main-content">
+        <div className="logo-container">
+          <img src="path-to-your-logo.png" alt="FireFit Logo" />
+          <p>물결처럼 흐르는 파란 장터</p>
+        </div>
+        <div className="form-container">
+          <button className="signup-button" onClick={handleSignupClick}>
+            회원가입
+          </button>
+          <button className="login-button" onClick={handleLoginClick}>
+            로그인
+          </button>
+          <a href="/forgot-password" className="forgot-password-link">
+            비밀번호를 잊으셨습니까?
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default First;
