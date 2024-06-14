691b9994871cb523dba2ad91a2f1d1392386efbe
becherryu <81617058+becherryu@users.noreply.github.com>
2024-06-14 14:29:50 +0900

    [FIX] 주소창 방지 막기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 //import Header from "./Header"; //공통헤더
@@ -22,12 +22,21 @@ import PostDetail from "./pages/FooterNav/Post/postDetail";
 import NickSetting from "./pages/User/nickSetting";
 import EditPost from "./pages/FooterNav/Post/editPost";
 import NotFoundPage from "./components/notFoundPage";
+import ProtectedRoute from "./protectedRoute"; // Import the ProtectedRoute component
+
 function App() {
   //우클릭 방지
   useEffect(() => {
     document.oncontextmenu = function () {
       return false;
     };
+  }, []);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("userToken");
+    setIsAuthenticated(!!userToken);
   }, []);
 
   const theme = createTheme({
@@ -53,28 +62,152 @@ function App() {
           <Routes>
             <Route path="/" element={<First />}></Route>
             <Route path="/login" element={<Login />}></Route>
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/chat" element={<Chat />}></Route>
-            <Route path="/favorite" element={<Favorite />}></Route>
-            <Route path="/writePost" element={<WritePost />}></Route>
-            <Route path="/search" element={<Search />}></Route>
-            <Route path="/settings" element={<Settings />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/mypage" element={<Mypage />}></Route>
-            <Route path="/buy" element={<Buy />}></Route>
-            <Route path="/sell" element={<Sell />}></Route>
-            <Route path="/post/:post_no" element={<PostDetail />}></Route>
-            <Route path="/report/:post_no" element={<Report />}></Route>
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute
+                  element={<Home />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/chat"
+              element={
+                <ProtectedRoute
+                  element={<Chat />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/favorite"
+              element={
+                <ProtectedRoute
+                  element={<Favorite />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/writePost"
+              element={
+                <ProtectedRoute
+                  element={<WritePost />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/search"
+              element={
+                <ProtectedRoute
+                  element={<Search />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/settings"
+              element={
+                <ProtectedRoute
+                  element={<Settings />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute
+                  element={<Profile />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/mypage"
+              element={
+                <ProtectedRoute
+                  element={<Mypage />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/buy"
+              element={
+                <ProtectedRoute
+                  element={<Buy />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/sell"
+              element={
+                <ProtectedRoute
+                  element={<Sell />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/post/:post_no"
+              element={
+                <ProtectedRoute
+                  element={<PostDetail />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/report/:post_no"
+              element={
+                <ProtectedRoute
+                  element={<Report />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
             <Route
               path="/chat/chatRoom/:chat_no"
-              element={<ChatRoom />}
-            ></Route>
-            <Route path="/editPost/:post_no" element={<EditPost />}></Route>
-            <Route path="/notifications" element={<Notifications />}></Route>
-            <Route path="/nickSetting" element={<NickSetting />}></Route>
-            {/* 모든 유효하지 않은 경로를 NotFoundPage로 리다이렉트
+              element={
+                <ProtectedRoute
+                  element={<ChatRoom />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/editPost/:post_no"
+              element={
+                <ProtectedRoute
+                  element={<EditPost />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/notifications"
+              element={
+                <ProtectedRoute
+                  element={<Notifications />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route
+              path="/nickSetting"
+              element={
+                <ProtectedRoute
+                  element={<NickSetting />}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
             <Route path="/404" element={<NotFoundPage />}></Route>
-            <Route path="*" element={<Navigate to="/404" replace />} /> */}
+            <Route path="*" element={<Navigate to="/404" replace />} />
           </Routes>
         </div>
       </BrowserRouter>
